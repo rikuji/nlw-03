7c981ba417cfbcf65d3c22dc33f9b3fa004a7fbc
lucas.valenzuela <lucas.valenzuela@squadra.com.br>
2020-10-14 07:16:37 -0300

    01:25:43

--- backend/src/models/Orphanage.ts
+++ backend/src/models/Orphanage.ts
@@ -1,9 +1,16 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  OneToMany,
+  JoinColumn,
+} from 'typeorm';
+import Image from './Image';
 
 @Entity('orphanages')
 export default class Orphanage {
   @PrimaryGeneratedColumn('increment')
-  id: string;
+  id: number;
 
   @Column()
   name: string;
@@ -15,7 +22,7 @@ export default class Orphanage {
   longitude: number;
 
   @Column()
-  about: string;
+  about: number;
 
   @Column()
   instructions: string;
@@ -25,4 +32,10 @@ export default class Orphanage {
 
   @Column()
   open_on_weekends: boolean;
+
+  @OneToMany(() => Image, (image) => image.orphanage, {
+    cascade: ['insert', 'update'],
+  })
+  @JoinColumn({ name: 'orphanage_id' })
+  images: Image[];
 }
